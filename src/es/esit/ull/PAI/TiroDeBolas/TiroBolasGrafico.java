005ce6536f4944f9484883b728719fdd27a5b4ee
alu0100774054 <alu0100774054@ull.edu.es>
2016-05-11 10:21:42 +0100

    tiro funciona

--- src/es/esit/ull/PAI/TiroDeBolas/TiroBolasGrafico.java
+++ src/es/esit/ull/PAI/TiroDeBolas/TiroBolasGrafico.java
@@ -21,7 +21,7 @@ public class TiroBolasGrafico extends JPanel {
   private ArrayList<Bola> bolasJuego;
   private ArrayList<Point> trazaTiro;
   private Timer timer;
-  private final int DELAY = 10;
+  private final int DELAY = 100;
   private boolean iniciado = false;
   private final int NUM_BOLAS = 10;
   private Random generadorAleatorios;
@@ -31,6 +31,10 @@ public class TiroBolasGrafico extends JPanel {
   private boolean cambiaRaton = false;
   private boolean disparado = false;
   private boolean mostrarTraza = false;
+  private Point inicio;
+  private int desplazamientoBola = 12;
+  private int actual = 0;
+  private boolean acabado = false;
   
   public TiroBolasGrafico() {
     bolaTiro = null;
@@ -41,9 +45,11 @@ public class TiroBolasGrafico extends JPanel {
     generadorAleatorios = new Random();
     raton = new Point();
     coordenadasBolaTiro = new Point();
+    inicio = new Point();
     establecerEstilo();
     generarColores();
     iniciarOyentes();
+    //timer.start();
   }
 
   private void iniciarOyentes() {
@@ -78,6 +84,7 @@ public class TiroBolasGrafico extends JPanel {
         dibujarFlecha(g);
       } else {
         dibujarBolas(g);
+        dibujarFlecha(g);
       }
       if (isDisparado()) {
         dibujarBolas(g);
@@ -86,22 +93,31 @@ public class TiroBolasGrafico extends JPanel {
       }
     }
   }
-  
+
   private void dibujarTraza(Graphics g) {
     if (isMostrarTraza()) {
       for (int i = 0; i < getTrazaTiro().size(); i++) {
         g.setColor(getBolaTiro().getFondo());
-        g.fillOval((int)getTrazaTiro().get(i).getX() - getBolaTiro().getRADIO() / 2, 
-            (int) getTrazaTiro().get(i).getY() - getBolaTiro().getRADIO() / 2, 
+        g.fillOval((int)getTrazaTiro().get(getActual()).getX() - getBolaTiro().getRADIO() / 2, 
+            (int) getTrazaTiro().get(getActual()).getY() - getBolaTiro().getRADIO() / 2, 
             (int) getBolaTiro().getRADIO(), 
             (int) getBolaTiro().getRADIO());
       }
     } else {
-      g.setColor(getBolaTiro().getFondo());
-      g.fillOval((int)getTrazaTiro().get(getTrazaTiro().size() - 1).getX() - getBolaTiro().getRADIO() / 2, 
-          (int) getTrazaTiro().get(getTrazaTiro().size() - 1).getY() - getBolaTiro().getRADIO() / 2, 
-          (int) getBolaTiro().getRADIO(), 
-          (int) getBolaTiro().getRADIO());
+      if (getActual() < getTrazaTiro().size() - 1) {
+        g.setColor(getBolaTiro().getFondo());
+        g.fillOval((int)getTrazaTiro().get(getActual()).getX() - getBolaTiro().getRADIO() / 2, 
+            (int) getTrazaTiro().get(getActual()).getY() - getBolaTiro().getRADIO() / 2, 
+            (int) getBolaTiro().getRADIO(), 
+            (int) getBolaTiro().getRADIO());
+        setActual(getActual() + 1);
+      } else {
+        g.setColor(getBolaTiro().getFondo());
+        g.fillOval((int)getTrazaTiro().get(getActual()).getX() - getBolaTiro().getRADIO() / 2, 
+            (int) getTrazaTiro().get(getActual()).getY() - getBolaTiro().getRADIO() / 2, 
+            (int) getBolaTiro().getRADIO(), 
+            (int) getBolaTiro().getRADIO());
+      }
     }
   }
 
@@ -109,15 +125,17 @@ public class TiroBolasGrafico extends JPanel {
     Graphics2D g2d = (Graphics2D)g;
     g.setColor(Color.BLACK);
     g2d.setStroke(new BasicStroke(5.0F));
-    System.out.println((int) getBolaTiro().getCentroX() + ", " +  (int) getBolaTiro().getCentroY() + "--- " + (int) getRaton().getX() + ", " + (int) getRaton().getY());
-    g2d.drawLine((int) getBolaTiro().getCentroX(), (int) getBolaTiro().getCentroY(), (int) getRaton().getX(), (int) getRaton().getY());
+    g2d.drawLine((int) this.getWidth() / 2,
+        (int) this.getHeight(), 
+        (int) getRaton().getX(), 
+        (int) getRaton().getY());
   }
 
   private void dibujarBolas(Graphics g) {
     if(!isIniciado()) {
       generarBolas();
     }
-    
+
     // Bolas del juego.
     for (int i = 0; i < getNUM_BOLAS(); i++) {
       g.setColor(getBolasJuego().get(i).getFondo());
@@ -126,17 +144,17 @@ public class TiroBolasGrafico extends JPanel {
           getBolasJuego().get(i).getRADIO(), 
           getBolasJuego().get(i).getRADIO());
     }
-    
+
     // Bolas del usuario.
     if(!isDisparado()) {
-    g.setColor(getBolaTiro().getFondo());
-    g.fillOval(getBolaTiro().getCentroX() - getBolaTiro().getRADIO() / 2, 
-        getBolaTiro().getCentroY() - getBolaTiro().getRADIO() / 2, 
-        getBolaTiro().getRADIO(), 
-        getBolaTiro().getRADIO());
+      g.setColor(getBolaTiro().getFondo());
+      g.fillOval(getBolaTiro().getCentroX() - getBolaTiro().getRADIO() / 2, 
+          getBolaTiro().getCentroY() - getBolaTiro().getRADIO() / 2, 
+          getBolaTiro().getRADIO(), 
+          getBolaTiro().getRADIO());
     }
   }
-  
+
   private void generarBolas() {
     // Bolas del juego.
     int radio = this.getWidth() / getNUM_BOLAS();
@@ -147,15 +165,35 @@ public class TiroBolasGrafico extends JPanel {
       getBolasJuego().add(new Bola(inicioX + radio / 2, inicioY, radio, getColoresJuego().get(colorAleatorio)));
       inicioX += radio;
     }
-    
+
     // Bola del usuario.
+    setInicio(new Point(this.getWidth() / 2, this.getHeight()));
     int colorAleatorio = getGeneradorAleatorios().nextInt(getColoresJuego().size() - 1);
-    setCoordenadasBolaTiro(new Point(this.getWidth() / 2 - radio / 2, this.getHeight() - radio / 2));
+    setCoordenadasBolaTiro(new Point((int) getInicio().getX(), (int) getInicio().getY()));
     setBolaTiro(new Bola((int) getCoordenadasBolaTiro().getX(),(int) getCoordenadasBolaTiro().getY(), radio, getColoresJuego().get(colorAleatorio)));
   }
-  
-  private void lanzar() {
-    
+
+  private void lanzar(int ratonX, int ratonY) {
+    int correcion = getBolaTiro().getRADIO() / 2;
+    System.out.println("punto origen " + getBolaTiro().getCentroX() + ", " + getBolaTiro().getCentroY() + " raton:" + ratonX + ", " + ratonY);
+    double pendiente = (float) (ratonY - getBolaTiro().getCentroY()) / (ratonX - getBolaTiro().getCentroX());
+    System.out.println("pendiente: " + pendiente);
+    while (getBolaTiro().getCentroY() >= getBolaTiro().getRADIO() + correcion ) {
+      Point nuevoPunto = null;
+      double nuevaY = getBolaTiro().getCentroY() - getDesplazamientoBola();
+      double nuevaX = ((nuevaY - getBolaTiro().getCentroY()) / pendiente) + getBolaTiro().getCentroX();    
+      System.out.println(nuevaX + ", " + nuevaY);
+      if (nuevaX <= getBolaTiro().getRADIO() / 2 || nuevaX >= (this.getHeight())) {
+        pendiente = -pendiente;
+        nuevaX = ((nuevaY - getBolaTiro().getCentroY()) / pendiente) + getBolaTiro().getCentroX();    
+      }
+      nuevoPunto = new Point((int) nuevaX, (int) nuevaY);
+      getTrazaTiro().add(nuevoPunto);
+      getBolaTiro().setCentroX((int) nuevoPunto.getX());
+      getBolaTiro().setCentroY((int) nuevoPunto.getY()); 
+    }
+    System.out.println(getBolaTiro().getCentroX() + ", " + getBolaTiro().getCentroY());
+       
   }
   class TimerListener implements ActionListener {
 
@@ -163,65 +201,100 @@ public class TiroBolasGrafico extends JPanel {
     public void actionPerformed(ActionEvent e) {
       repaint();
     }
-    
+
   }
-  
+
   class RatonListener implements MouseMotionListener {
 
     @Override
     public void mouseDragged(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-      setRaton(new Point(e.getX(), e.getY()));
-      cambiaRaton = true;
-      repaint();
+      if (!disparado) {
+        setRaton(new Point(e.getX(), e.getY()));
+        cambiaRaton = true;
+        repaint();
+        cambiaRaton = false;
+      }   
     }
-    
   }
-  
+
   class LanzarListener implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-      lanzar();
+      lanzar(e.getX(), e.getY());
       setDisparado(true);
-      repaint();
+      getTimer().start();
+      //repaint();
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
     }
-    
+
   }
-  
-  
-  
-  
-  
+
+
+
+
+
+  public boolean isAcabado() {
+    return acabado;
+  }
+
+  public void setAcabado(boolean acabado) {
+    this.acabado = acabado;
+  }
+
+  public int getActual() {
+    return actual;
+  }
+
+  public void setActual(int actual) {
+    this.actual = actual;
+  }
+
+  public int getDesplazamientoBola() {
+    return desplazamientoBola;
+  }
+
+  public void setDesplazamientoBola(int desplazamientoBola) {
+    this.desplazamientoBola = desplazamientoBola;
+  }
+
+  public Point getInicio() {
+    return inicio;
+  }
+
+  public void setInicio(Point inicio) {
+    this.inicio = inicio;
+  }
+
   public boolean isMostrarTraza() {
     return mostrarTraza;
   }
@@ -325,6 +398,6 @@ public class TiroBolasGrafico extends JPanel {
   public int getDELAY() {
     return DELAY;
   }
-  
-  
+
+
 }
